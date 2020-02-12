e795afb35c22b05a8b105eb07fba8732fc30d3f1
Jamie Shaw <proxeh@gmail.com>
2020-02-12 23:11:47 +0000

    Updates

---
+++ precache-manifest.4847a6993bcf670a51a23b28a0846fcd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6839db98eb4eecb93df22e0cdce30a06",
+    "url": "/Bootstrap/index.html"
+  },
+  {
+    "revision": "016982b1cf7e0a0e1b3b",
+    "url": "/Bootstrap/static/css/main.527593a1.chunk.css"
+  },
+  {
+    "revision": "aea8e7be5cbd8d2936a5",
+    "url": "/Bootstrap/static/js/2.0132ac69.chunk.js"
+  },
+  {
+    "revision": "a52d65c95a2d7ffcb9ff212c4d38dff4",
+    "url": "/Bootstrap/static/js/2.0132ac69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "016982b1cf7e0a0e1b3b",
+    "url": "/Bootstrap/static/js/main.9d04c17f.chunk.js"
+  },
+  {
+    "revision": "9c126218e5a9ca16747b",
+    "url": "/Bootstrap/static/js/runtime-main.20460c84.js"
+  }
+]);
